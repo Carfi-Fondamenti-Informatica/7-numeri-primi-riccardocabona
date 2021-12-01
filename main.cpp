98516b9e664532ae8d624f614d4c8a2def557cfe
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-01 17:48:26 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n=0;
+    cin>>n;
+    if(primo(n,2)==true){
+        cout<<"numero primo";
+    }
+    else{
+        cout<<"numero NON primo";
+    }
+    return 0;
 }
+
