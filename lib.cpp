318cad031d048a95cfe84e0a67f4c5d11a3d5f8d
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-01 17:49:50 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool primo(int &n,int i){
+    if(n==i){
+        return true;
+    }
+    else if(n%i==0 or n==1){
+        return false;
+    }
+    else{
+        return primo(n,i+1);
+    }
+}
